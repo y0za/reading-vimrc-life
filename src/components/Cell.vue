6ba1d11aa457d869c5966a7d5ae8741ec8c5e7a8
y0za <yoza.is12s@gmail.com>
2017-02-20 21:00:56 +0900

    Add Cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,29 @@
+<template>
+  <rect width="15" height="15"
+    v-bind:x="x"
+    v-bind:y="y"
+    v-bind:fill="fill"></rect>
+</template>
+
+<script>
+export default {
+  name: 'cell',
+  computed: {
+    x() {
+      return 50 + (20 * this.column)
+    },
+    y() {
+      return 50 + (20 * this.row)
+    },
+    fill() {
+      return this.alive ? '#6FF118' : '#999'
+    }
+  },
+  props: {
+    alive: Boolean,
+    index: Number,
+    row: Number,
+    column: Number
+  }
+}
+</script>
