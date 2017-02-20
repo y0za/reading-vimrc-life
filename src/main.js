d4bb03cf7c7c816834cf90048260d0b88c674f98
y0za <yoza.is12s@gmail.com>
2017-02-20 21:03:47 +0900

    Add extracting process before render Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+function extractPaticipations() {
+  const rectList = document.querySelectorAll('#d3-participation-streak-graph rect')
+  const participations = Array.prototype.map.call(rectList, (rect) => {
+    return rect.attributes.fill.value !== '#999'
+  })
+  return participations
+}
+
+function init() {
+  const dd = document.querySelector('#d3-participation-streak-graph')
+
+  store.commit('setInitialState', {
+    cells: extractPaticipations(),
+    rowCount: 10
+  })
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: dd,
+    render: h => h(App),
+    store
+  })
+  /* eslint-enable no-new */
+}
+
+document.addEventListener('DOMContentLoaded', init)
+
