8aa8f55a82da178e8e40314bb50dee60d82f109c
y0za <yoza.is12s@gmail.com>
2017-02-20 21:01:19 +0900

    Change App component to represent Game of Life

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <dd id="d3-participation-streak-graph">
+    <svg width="650" height="300">
+      <template v-for="(row, y) in field">
+        <cell v-for="(alive, x) in row"
+          v-bind:alive="alive"
+          v-bind:row="y"
+          v-bind:column="x"></cell>
+      </template>
+    </svg>
+  </dd>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Cell from './components/Cell.vue'
+
 export default {
   name: 'app',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  computed: mapState({
+    field: state => state.field
+  }),
+  components: {
+    Cell
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
