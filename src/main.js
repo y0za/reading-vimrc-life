f99790383a81842ff2c4aede93eef69bf723cad1
y0za <yoza.is12s@gmail.com>
2017-02-22 00:56:07 +0900

    Change trigger process of main.js

--- src/main.js
+++ src/main.js
@@ -3,30 +3,46 @@ import App from './App.vue'
 import store from './store'
 import * as mTypes from './store/mutation-types'
 
-function extractPaticipations() {
-  const rectList = document.querySelectorAll('#d3-participation-streak-graph rect')
-  const participations = Array.prototype.map.call(rectList, (rect) => {
-    return rect.attributes.fill.value !== '#999'
-  })
-  return participations
-}
-
-function init() {
-  const dd = document.querySelector('#d3-participation-streak-graph')
-
+function prepareLife(streakGraph, participations) {
   store.commit(mTypes.SET_INITIAL_STATE, {
-    cells: extractPaticipations(),
+    cells: participations,
     rowCount: 10
   })
 
   /* eslint-disable no-new */
   new Vue({
-    el: dd,
+    el: streakGraph,
     render: h => h(App),
     store
   })
   /* eslint-enable no-new */
 }
 
-document.addEventListener('DOMContentLoaded', init)
+function extractPaticipations(rectList) {
+  const participations = Array.prototype.map.call(rectList, (rect) => {
+    return rect.attributes.fill.value !== '#999'
+  })
+  return participations
+}
 
+const streakGraphId = 'd3-participation-streak-graph'
+
+const observer = new MutationObserver((mutations) => {
+  const rectList = document.querySelectorAll(`#${streakGraphId} rect`)
+  if (rectList.length === 0) {
+    return
+  }
+
+  const streakGraph = document.getElementById(streakGraphId)
+  const participations = extractPaticipations(rectList)
+  prepareLife(streakGraph, participations)
+
+  observer.disconnect()
+})
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false
+})
