42fead7cb0ce2e81be14cd82d89153a9bca211ff
y0za <yoza.is12s@gmail.com>
2017-02-22 22:58:22 +0900

    Add ScaleMark component

---
+++ src/components/ScaleMark.vue
@@ -0,0 +1,26 @@
+<template>
+  <g style="font-size: 10px;">
+    <text v-bind:x="x"
+          y="30"
+          dy=".71em"
+          fill="steelblue"
+          style="text-anchor: middle;">
+      {{ scaleValue }}
+    </text>
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'scale-mark',
+  computed: {
+    x() {
+      return 50 + (40 * this.index)
+    }
+  },
+  props: {
+    scaleValue: Number,
+    index: Number
+  }
+}
+</script>
