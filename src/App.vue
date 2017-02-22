85b8c5101c158307aa3e006ac6f729393f75541d
y0za <yoza.is12s@gmail.com>
2017-02-22 22:47:26 +0900

    Tweak App.vue template indent

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <svg width="650" height="300">
       <template v-for="(row, y) in field">
         <cell v-for="(alive, x) in row"
-          v-bind:alive="alive"
-          v-bind:row="y"
-          v-bind:column="x"></cell>
+              v-bind:alive="alive"
+              v-bind:row="y"
+              v-bind:column="x"></cell>
       </template>
     </svg>
     <div class="gol-controller">
